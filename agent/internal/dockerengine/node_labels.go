48e9fffab0381357134be60567e0dd527d5f261f
Hyung gyun Kim <157089559+KimHG1995@users.noreply.github.com>
2026-09-23 10:12:41 +0900

    style: format node label mutator

--- agent/internal/dockerengine/node_labels.go
+++ agent/internal/dockerengine/node_labels.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"sort"
 
-	cerrdefs "github.com/containerd/errdefs"
 	"github.com/KimHG1995/Docklane/agent/internal/model"
+	cerrdefs "github.com/containerd/errdefs"
 	"github.com/moby/moby/api/types/swarm"
 	"github.com/moby/moby/client"
 )
